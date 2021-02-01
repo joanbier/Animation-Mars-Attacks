80ff5cc45d9fe458226166314f2f3b2b836984b6
Joanna Biernat <joannabiernat@MacBook-Pro-Joanna.local>
2021-02-01 18:05:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const elements = document.querySelectorAll('.anim-bg')
+
+    document.addEventListener('mousemove', function (event) {
+        const {
+            clientX,
+            clientY
+        } = event;
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+
+        elements.forEach(element => {
+
+            const ratioX = -element.getAttribute('ratioX');
+            const ratioY = -element.getAttribute('ratioY');
+
+            const moveX = clientX - centerX;
+            const moveY = clientY - centerY;
+
+            element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`
+        })
+    })
+
+})
